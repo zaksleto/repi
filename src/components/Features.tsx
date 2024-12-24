b0b5f984223cc51b78e28fb143c7f15d321abbc9
zaksleto <44687194+zaksleto@users.noreply.github.com>
2024-12-23 21:32:12 -0300

    Update Features.tsx

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -26,7 +26,7 @@ const features = [
 
 export default function Features() {
   return (
-    <div id="features" className="bg-gray-50 py-24">
+    <div id="features" className="bg-gray-50 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
