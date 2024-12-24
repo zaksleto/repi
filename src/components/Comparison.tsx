2f5001bd2473cc9005f2961b4f2dd4a17c53528a
zaksleto <44687194+zaksleto@users.noreply.github.com>
2024-12-23 21:34:59 -0300

    Update Comparison.tsx

--- src/components/Comparison.tsx
+++ src/components/Comparison.tsx
@@ -5,7 +5,7 @@ import { comparisonPoints } from '../data/comparisonData';
 
 export default function Comparison() {
   return (
-    <div id="comparison" className="py-24 bg-white">
+    <div id="comparison" className="py-8 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
